8732bf2c58887dc3cef49c0336b528f7214a0e20
detnon <samueldetnon@gmail.com>
2020-04-01 17:27:08 +0100

    added some more

--- kindergarten-garden/kindergarten_garden.py
+++ kindergarten-garden/kindergarten_garden.py
@@ -3,14 +3,13 @@ class Garden:
     def get_groups(self, diagram):
         rows = diagram.split('\n')
         groups = []
-        f = 0
-        s = 2
-        for r in rows:
+        f=0
+        s=2
+        for r in range(int(len(rows[0]) / 2)):
             groups.append(''.join(rows[0][f:s]+rows[1][f:s]))
-            f += 2
-            s += 2
+            f+=2
+            s+=2
         return groups
-
 
 
     def plants(self, student):
@@ -20,22 +19,21 @@ class Garden:
             'C':'Clover',
             'V':'Violets'}
 
+        print( self.get_groups(self.diagram))
         group_index = self.students.index(student)
-        print(group_index)
+        print( group_index)
         student_group = self.get_groups(self.diagram)[group_index]
-        print(student_group)
         students_plants = []
 
         for i in student_group:
-            print(i)
             for k, v in list.items():
                 if i == k:
                     students_plants.append(v)
-
-        print(students_plants)
         return students_plants
 
 
-    def __init__(self, diagram, students):
+    def __init__(self, diagram, students=['Alice','Bob','Charlie','David',
+                                          'Eve','Fred','Ginny','Harriet',
+                                          'Llena','Joseph','Kincaid','Larry']):
         self.students = sorted(students)
         self.diagram = diagram
